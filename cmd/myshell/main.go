9af398baf6def59fb3b3b28ad9ab208b93d11188
Mohammad <mohammadkhakharia@gmail.com>
2024-05-24 19:30:46 -0400

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,9 +7,14 @@ import (
 	"strings"
 )
 
+var cmds = make(map[string]string)
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	//fmt.Println("Logs from your program will appear here!")
+	cmds["exit"] = "builtin"
+	cmds["type"] = "builtin"
+	cmds["echo"] = "builtin"
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		// Wait for user input
@@ -18,16 +23,26 @@ func main() {
 
 		//checking if command exists
 		switch trimmedCommand[0] {
+
 		case "exit":
 			if trimmedCommand[1] == "0" {
 				os.Exit(0)
 			}
+
 		case "echo":
 			res := ""
 			for i := 1; i < len(trimmedCommand); i++ {
 				res += " " + trimmedCommand[i]
 			}
 			fmt.Print(res + "\n")
+
+		case "type":
+			t, ok := cmds[trimmedCommand[1]]
+			if ok {
+				fmt.Print(trimmedCommand[1] + " is a shell " + t)
+			} else {
+				fmt.Print(trimmedCommand[1] + " not found")
+			}
 
 		default:
 			fmt.Print(trimmedCommand[0] + ": command not found\n")
