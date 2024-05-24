5baa6b0a89f39c73aeb6920b5e04018636b7728a
Mohammad <mohammadkhakharia@gmail.com>
2024-05-24 18:58:21 -0400

    pass stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,13 +10,16 @@ import (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	//fmt.Println("Logs from your program will appear here!")
-	fmt.Fprint(os.Stdout, "$ ")
-	// Wait for user input
-	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	trimmedCommand := strings.TrimSpace(command)
-	//checking if command exists
-	switch command {
-	default:
-		fmt.Print(trimmedCommand + ": command not found\n")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		// Wait for user input
+		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		trimmedCommand := strings.TrimSpace(command)
+		//checking if command exists
+		switch command {
+		default:
+			fmt.Print(trimmedCommand + ": command not found\n")
+		}
 	}
+
 }
